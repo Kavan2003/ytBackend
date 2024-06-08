3b96d3d454552b32ba4a43228589cbf892fb0726
Kavan <kavanthosani2003@gmail.com>
2024-06-08 11:59:13 +0530

    chore: Update user.routes.js
    The code changes include updating the user.routes.js file to add new routes for user authentication, profile management, and avatar/cover image updates. This improves the functionality and user experience of the application.

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,46 +4,71 @@ import Joi from "joi";
 import { User } from "../models/user.models.js";
 import { uploadFileOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-const schema = Joi.object({
-  username: Joi.string().required().messages({
-    "any.required": "Username is required",
-  }),
-  email: Joi.string().email().required().messages({
-    "any.required": "Email is required",
-    "string.email": "Invalid email format",
-  }),
-  fullName: Joi.string().required().messages({
-    "any.required": "Full name is required",
-  }),
-  password: Joi.string().min(6).required().messages({
-    "any.required": "Password is required",
-    "string.min": "Password should be at least 6 characters",
-  }),
-  bio: Joi.string().max(100).messages({
-    "string.max": "Bio should not exceed 100 characters",
-  }),
-  websites: Joi.array().items(Joi.string().uri()).messages({
-    "array.base": "Websites should be an array",
-    "string.uri": "Invalid website URL format",
-  }),
-  avatarlocalfile: Joi.string().required().messages({
-    "any.required": "Avatar is required",
-  }),
-});
+import { statusCodes } from "../constants.js";
+
+const generateRefreshTokenAndAccessToken = async (userid) => {
+  try {
+    const user = await User.findById(userid);
+    if (!user) {
+      throw new ApiError(
+        statusCodes.NOT_FOUND,
+        "User not found while generating tokens"
+      );
+    }
+    const refreshToken = user.generateRefreshToken();
+    const accessToken = user.generateAccessToken();
+
+    await user.save({ validateBeforeSave: false });
+
+    return { refreshToken, accessToken };
+  } catch (error) {
+    console.log("Error in generating tokens ", error);
+    throw new ApiError(
+      statusCodes.INTERNAL_SERVER_ERROR,
+      "Error in generating tokens ",
+      error.message
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   const { username, email, fullName, password, bio, websites } = req.body;
 
-  console.log("req.files", req.files);
   const avatarlocalfile = req.files?.avatar[0]?.path;
 
-  // console.log("req", req.body);
   var coverlocalfile;
   if (req.files?.cover) {
     coverlocalfile = req.files?.cover[0]?.path;
   } else {
     coverlocalfile = "";
   }
+
+  const schema = Joi.object({
+    username: Joi.string().required().messages({
+      "any.required": "Username is required",
+    }),
+    email: Joi.string().email().required().messages({
+      "any.required": "Email is required",
+      "string.email": "Invalid email format",
+    }),
+    fullName: Joi.string().required().messages({
+      "any.required": "Full name is required",
+    }),
+    password: Joi.string().min(6).required().messages({
+      "any.required": "Password is required",
+      "string.min": "Password should be at least 6 characters",
+    }),
+    bio: Joi.string().max(100).messages({
+      "string.max": "Bio should not exceed 100 characters",
+    }),
+    websites: Joi.array().items(Joi.string().uri()).messages({
+      "array.base": "Websites should be an array",
+      "string.uri": "Invalid website URL format",
+    }),
+    avatarlocalfile: Joi.string().required().messages({
+      "any.required": "Avatar is required",
+    }),
+  });
 
   const { error } = schema.validate(
     { username, email, fullName, password, bio, websites, avatarlocalfile },
@@ -52,7 +77,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (error) {
     throw new ApiError(
-      400,
+      statusCodes.BAD_REQUEST,
       error.details.map((detail) => detail.message)
     );
   } else {
@@ -61,7 +86,10 @@ const registerUser = asyncHandler(async (req, res) => {
     });
     if (extingUser) {
       console.log("User already exists");
-      throw new ApiError(409, "Username or email already exists");
+      throw new ApiError(
+        statusCodes.Already_Exists,
+        "Username or email already exists"
+      );
     }
 
     const avatarurl = await uploadFileOnCloudinary(avatarlocalfile);
@@ -88,15 +116,200 @@ const registerUser = asyncHandler(async (req, res) => {
     );
 
     if (!createdUser) {
-      throw new ApiError(500, "Error in creating user");
+      throw new ApiError(
+        statusCodes.INTERNAL_SERVER_ERROR,
+        "Error in creating user"
+      );
     }
     console.log("User registered successfully");
 
     // return res.send(new ApiResponse(201, "User registered successfully", createdUser));
     return res
-      .status(201)
+      .status(statusCodes.CREATED)
       .json(new ApiResponse(201, "User registered successfully", createdUser));
   }
 });
+const loginUser = asyncHandler(async (req, res) => {
+  //  take data from user req.body
+  // validate the data using Email/Username and password
+  // if user exists, generate token and send it to user
+  // if user does not exist, send error message
+  // do refresh token logic
+  // send response to user and cookie
 
-export { registerUser };
+  const { email, username, password } = req.body;
+  // if(!email && !username){
+  //   throw new ApiError(statusCodes.BAD_REQUEST, "Email or username is required");
+  // }
+  const Schema = Joi.object({
+    email: Joi.string().email().messages({
+      "string.email": "Invalid email format",
+    }),
+    username: Joi.string().messages({
+      "string.base": "Invalid username format",
+    }),
+    password: Joi.string().required().min(6).messages({
+      "any.required": "Password is required",
+
+      "string.min": "Password should be at least 6 characters",
+    }),
+  })
+    .or("email", "username")
+    .messages({
+      "object.missing": "Either Email or username is required",
+    });
+  const { error } = Schema.validate(
+    { email, username, password },
+    { abortEarly: false }
+  );
+  if (error) {
+    // console.log("error", error);
+    throw new ApiError(
+      statusCodes.BAD_REQUEST,
+      error.details.map((detail) => detail.message)
+    );
+  }
+  const user = await User.findOne({ $or: [{ email }, { username }] });
+  if (!user) {
+    throw new ApiError(statusCodes.NOT_FOUND, "User not found");
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    throw new ApiError(statusCodes.UNAUTHORIZED, "Invalid credentials");
+  }
+  const { refreshToken, accessToken } =
+    await generateRefreshTokenAndAccessToken(user._id);
+
+  const loggedUser = await User.findById(user._id).select("-password -refreshToken");
+
+  const cookieOptions = {
+    httpOnly: true,
+    secure: true,
+  };
+  res
+    .status(statusCodes.OK)
+    .cookie("refreshToken", refreshToken, cookieOptions)
+    .cookie("accessToken", accessToken, cookieOptions)
+    .json(
+      new ApiResponse(statusCodes.OK, "Success", {
+        loggedUser,
+        accessToken,
+        refreshToken,
+      })
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    { refreshToken: undefined },
+    { new: true }
+  );
+  const cookieOptions = {
+    httpOnly: true,
+    secure: true,
+  };
+  res
+    .status(statusCodes.OK)
+    .clearCookie("refreshToken", cookieOptions)
+    .clearCookie("accessToken", cookieOptions)
+    .json(new ApiResponse(statusCodes.OK, "Logged out successfully", {}));
+});
+
+const RefreshAccessToken = asyncHandler(async (req, res) => {
+  const { incommingRefreshToken } = req.cookies?.refreshToken||req.headers.authorization.replace("Bearer ", "");
+  if (!incommingRefreshToken) {
+    throw new ApiError(statusCodes.BAD_REQUEST, "Refresh token is required");
+  }
+  const decodeToken = jwt.verify(incommingRefreshToken, process.env.REFRESH_TOKEN);
+  const user = await User.findById(decodeToken.id);
+  if (!user) {
+    throw new ApiError(statusCodes.NOT_FOUND, "User not found");
+  }
+
+  const { refreshToken, accessToken } = await generateRefreshTokenAndAccessToken();
+  res.status(statusCodes.OK).json(new ApiResponse(statusCodes.OK, "Success", { refreshToken, accessToken}));
+});
+
+const updateProfile = asyncHandler(async (req, res) => {
+  const { username,  fullName, bio, websites } = req.body;
+
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(statusCodes.NOT_FOUND, "User not found");
+  }
+
+  user.username = username || user.username;
+  user.fullName = fullName || user.fullName;
+  user.bio = bio || user.bio;
+  user.websites = websites || user.websites;
+
+  await user.save({ validateBeforeSave: false});
+
+  res.status(statusCodes.OK).json(new ApiResponse(statusCodes.OK, "Profile updated successfully", user));
+});
+const changePassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user._id);
+  if (!user) {
+    throw new ApiError(statusCodes.NOT_FOUND, "User not found");
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    throw new ApiError(statusCodes.UNAUTHORIZED, "Invalid Old Password");
+  }
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res.status(statusCodes.OK).json(new ApiResponse(statusCodes.OK, "Password changed successfully",{}));
+
+
+
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  res.status(statusCodes.OK).json(new ApiResponse(statusCodes.OK, "Success", req.user));
+});
+
+const UpdateAvatarOrCover = asyncHandler(async (req, res) => {
+  // const localflieAvatar = req.files?.avatar[0]?.path;
+
+  
+  var Avatarlocalfile;
+  if (req.files?.avatar) {
+    Avatarlocalfile = req.files?.avatar[0]?.path;
+  } else {
+    Avatarlocalfile = "";
+  }
+
+  
+  var coverlocalfile;
+  if (req.files?.cover) {
+    coverlocalfile = req.files?.cover[0]?.path;
+  } else {
+    coverlocalfile = "";
+  }
+
+if(Avatarlocalfile){
+  const avatarurl = await uploadFileOnCloudinary(Avatarlocalfile);
+  console.log("avatarurl=", avatarurl);
+}
+if(coverlocalfile){
+  const coverurl = await uploadFileOnCloudinary(coverlocalfile);
+  console.log("coverurl=", coverurl);
+}
+  
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      throw new ApiError(statusCodes.NOT_FOUND, "User not found");
+    }
+    user.avatar = avatarurl?.url || user.avatar;
+    user.coverImage = coverurl?.url || user.coverImage;
+    await user.save({ validateBeforeSave: false });
+    res.status(statusCodes.OK).json(new ApiResponse(statusCodes.OK, "Profile  updated successfully", user));
+
+});
+
+
+export { registerUser, loginUser, logoutUser, RefreshAccessToken,updateProfile,changePassword,getCurrentUser,UpdateAvatarOrCover };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,42 @@
 import { Router } from "express";
-import { registerUser} from "../controllers/user.controllers.js"
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  RefreshAccessToken,
+  updateProfile,
+  changePassword,
+  getCurrentUser,
+  UpdateAvatarOrCover,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
+import { varifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
 router.route("/register").post(
-    
-    upload.fields([
-        { name: 'avatar', maxCount: 1 },
-        { name: 'cover', maxCount: 1 }
-      ])
-    ,registerUser);   
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "cover", maxCount: 1 },
+  ]),
+  registerUser
+);
+router.route("/login").post(upload.none(), loginUser);
+router.route("/profile").get(varifyJWT, getCurrentUser);
+router.route("/profile").put(varifyJWT, updateProfile);
+router.route("/change-password").put(varifyJWT, changePassword);
+router.route("/update-avatar-cover").put(
+  varifyJWT,
+  upload.fields([
+    { name: "avatar", maxCount: 1 },
+    { name: "cover", maxCount: 1 },
+  ]),
+  UpdateAvatarOrCover
+);
+
+//secure routes
+
+router.route("/logout").post(varifyJWT, logoutUser);
+router.route("/refresh").post(RefreshAccessToken);
 
 export default router;
